623bd1ce92a70f2041b615d80db98d72fd86ea07
Viana2093 <viana2093@gmail.com>
2023-04-13 17:31:12 -0500

    Flyout

--- MAUITest/MAUITest/App.xaml.cs
+++ MAUITest/MAUITest/App.xaml.cs
@@ -8,11 +8,13 @@ public partial class App : Application
 
         //MainPage = new ContentPageDemo();
         //MainPage = new NavigationPage(new MainPage());
-        var navPage = new NavigationPage(new ContentPageDemo());
-        navPage.BarBackgroundColor = Colors.Chocolate;
-        navPage.BarTextColor = Colors.White;
-        navPage.Title = "Demo MAUI";
-        MainPage = navPage;
+        //var navPage = new NavigationPage(new ContentPageDemo());
+        //navPage.BarBackgroundColor = Colors.Chocolate;
+        //navPage.BarTextColor = Colors.White;
+        //navPage.Title = "Demo MAUI";
+        //MainPage = navPage;
+
+        MainPage = new FlyoutPageDemo();
 
     }
 }
